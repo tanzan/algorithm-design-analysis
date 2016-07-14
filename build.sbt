f96358af7f5485b5f5d82a7d5e983a1507839339
Serg Zubovich <serg.p.zubovich@gmail>
2016-07-14 23:21:00 +0300

    start of array inversions

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "algorithm-design-analysis"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ src/main/scala/Inversions.scala
@@ -0,0 +1,31 @@
+import scala.io.Source
+
+/**
+  * Created by serg on 14.07.16.
+  */
+object Inversions {
+
+  def readFromFile(fileName:String):Array[Int] =
+    Source.fromFile(fileName).getLines().map(_.toInt).toArray
+
+  def count(input: Array[Int]):Int = {
+
+    def countAndSort(input:Array[Int], start:Int, end:Int, output:Array[Int]):Int = {
+
+      val mid = (end - start) / 2
+
+      val left = countAndSort(input, start, mid, output)
+      val right = countAndSort(input, mid, end, output)
+      val split = mergeAndCountSplits(input, start, end, output)
+
+      left + right + split
+    }
+
+    def mergeAndCountSplits(input:Array[Int], start:Int, end:Int, output:Array[Int]):Int = {
+
+    }
+
+    countAndSort(input, 0, input.size, Array.ofDim(input.length))
+  }
+
+}
