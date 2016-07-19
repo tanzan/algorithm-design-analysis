f7cd5696f62e8e5ba753d6f4325f051254994a47
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2016-07-19 18:15:09 +0300

    complete quicksort

--- src/main/scala/QuickSort.scala
+++ src/main/scala/QuickSort.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 /**
   * Created by serg on 18.07.16.
   */
@@ -5,7 +7,8 @@
 object QuickSort {
 
 
-  def sort(input:Array[Int], start:Int, end:Int, choosePivot:(Int, Int, Array[Int]) => Int):Unit = {
+  def sort(input:Array[Int], start:Int, end:Int,
+           choosePivot:(Int, Int, Array[Int]) => Int):Int = {
 
     def swap(i:Int, j:Int):Unit = {
       val tmp = input(i)
@@ -13,30 +16,64 @@ object QuickSort {
       input(j) = tmp
     }
 
-    if (end - start <= 1) ()
+    if (end - start <= 1) 0
     else {
       val pivot = choosePivot(start, end, input)
+      swap(start, pivot)
       var i = start + 1
 
       for (j <- start until end) {
-        if (input(j) < input(pivot)) {
+        if (input(j) < input(start)) {
           swap(i, j)
           i += 1
         }
       }
-      swap(pivot, i - 1)
+      swap(start, i - 1)
 
-      sort(input, start, i - 1, choosePivot)
-      sort(input, i, end, choosePivot)
+      end - start - 1 +
+        sort(input, start, i - 1 , choosePivot) +
+        sort(input, i , end, choosePivot)
     }
   }
 
-  def sortWithStartPivot(input:Array[Int]):Unit = sort(input, 0, input.length, (start, end, input) => start)
+  def sortWith(array:Array[Int], choosePivot:(Int, Int, Array[Int]) => Int):Int =
+    sort(array, 0, array.length, choosePivot)
+
+  def startPivot(start:Int, end:Int, input:Array[Int]):Int = start
+
+  def endPivot(start:Int, end:Int, input:Array[Int]):Int = end - 1
+
+  def medianPivot(start:Int, end:Int, input:Array[Int]):Int = {
+    val mid =  start + (end - start + 1)/2  - 1
+    if (input(start) < input(end - 1)) {
+      if (input(mid) < input(start)) start
+      else if (input(mid) > input(end - 1)) end - 1
+      else mid
+    } else {
+      if (input(mid) < input(end - 1)) end - 1
+      else if(input(mid) > input(start)) start
+      else mid
+    }
+  }
+
+  val rng = Random
+
+  def randomPivot(start:Int, end:Int, input:Array[Int]):Int = start + rng.nextInt(end - start)
 
   def main(args: Array[String]) {
     val input = Util.readFromFile("QuickSort.txt")
-    sortWithStartPivot(input)
-    input.foreach(println)
+    val a1 = input.map(x => x)
+    val a2 = input.map(x => x)
+    val a3 = input.map(x => x)
+    val a4 = input.map(x => x).sortWith(_ < _)
+    val a5 = input.map(x => x)
+
+    println(sortWith(a1, startPivot))
+    println(sortWith(a2, endPivot))
+    println(sortWith(a3, medianPivot))
+    println(sortWith(a5, randomPivot))
+
+    println(input.indices.forall(i => a1(i) == a2(i) && a2(i) == a3(i) && a3(i) == a4(i) && a5(i) == a4(i)))
   }
 
 }
