c8c64208470829e655857a168d7848a6438b7f17
Serg Zubovich <serg.p.zubovich@gmail>
2016-08-18 21:52:15 +0300

    single link clustering

--- src/main/scala/algo/greedy/SingleLinkClustering.scala
+++ src/main/scala/algo/greedy/SingleLinkClustering.scala
@@ -11,7 +11,11 @@ object SingleLinkClustering {
 
   class CVertex(val id:Int, var leader:Int, val followers:mutable.Set[CVertex] = mutable.Set.empty) extends Vertex
 
-  implicit def createVertex(id:Int):CVertex = new CVertex(id, id)
+  implicit def createVertex(id:Int):CVertex =  {
+    val v = new CVertex(id, id)
+    v.followers += v
+    v
+  }
 
   def kClustering(graph: Graph[CVertex], k:Int):(Int, Set[CVertex]) = {
     var queue = graph.edges.toVector.sortWith(_.weight < _.weight)
@@ -27,11 +31,13 @@ object SingleLinkClustering {
         val leader2 = graph.vertex(edge.v2.leader).get
         if (leader1.followers.size < leader2.followers.size) {
           leader1.followers.foreach(v => v.leader = leader2.id)
+          leader1.leader = leader2.id
           leader2.followers ++= leader1.followers
           leader1.followers.clear()
           clusters -= leader1
         } else {
           leader2.followers.foreach(v => v.leader = leader1.id)
+          leader2.leader = leader1.id
           leader1.followers ++= leader2.followers
           leader2.followers.clear()
           clusters -= leader2
@@ -43,9 +49,10 @@ object SingleLinkClustering {
 
   def main(args: Array[String]): Unit = {
     val g = Graph.read("clustering1.txt")
-    val res = kClustering(g, 4)
+    val res = kClustering(g, 5)
+    println(g.vertices.size)
     println(res._1)
-    println(res._2.map(_.leader).toVector)
+    println(res._2.map(_.followers.map(_.id)).toVector)
   }
 
 }
