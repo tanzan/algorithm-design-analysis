3fc3784d347a0d159be4ee02f83c5e79d190c021
Serg Zubovich <serg.p.zubovich@gmail>
2016-08-20 18:24:30 +0300

    hypercube single link clustering

--- src/main/scala/algo/greedy/SingleLinkClustering.scala
+++ src/main/scala/algo/greedy/SingleLinkClustering.scala
@@ -1,8 +1,9 @@
 package algo.greedy
 
-import algo.graph.{Graph, Vertex, Edge}
+import algo.graph.{Edge, Graph, Vertex}
 
 import scala.collection.mutable
+import scala.io.Source
 
 /**
   * Created by serg on 18.08.16.
@@ -23,6 +24,7 @@ object SingleLinkClustering {
     v.followers += v
     v
   }
+
 
   def kClustering(graph: Graph[CVertex], k:Int):(Int, Set[CVertex]) = {
     var queue = graph.edges.toVector.sortWith(_.weight < _.weight)
@@ -61,10 +63,55 @@ object SingleLinkClustering {
 
   }
 
+
+  final val LabelSize = 24
+
+  def readHypercube(fileName:String):Iterable[CVertex] =
+    (for(line <- Source.fromFile(fileName).getLines().drop(1))
+      yield createVertex(Integer.parseInt(line.replace(" ", ""), 2))).toIterable
+
+
+  def numOfHypercubeClusters(vertices:Iterable[CVertex], minSpacing:Int):Int = {
+
+    val vertexMap:Map[Int, CVertex] = vertices.map(v => v.id -> v).toMap
+
+    def bitsToInt(bits:Vector[Int]):Int =
+      bits.zipWithIndex.foldLeft(0)((n, b) => n | (b._1 << b._2))
+
+    def edgesWithDistance(dist:Int):Iterable[Edge[CVertex]] = {
+      val diffs = (Vector.fill(dist)(1) ++ Vector.fill(LabelSize - dist)(0))
+        .permutations.map(x => bitsToInt(x)).toIterable
+      for(d <- diffs; v <- vertices if vertexMap.contains(v.id ^ d))
+        yield Edge[CVertex](v, vertexMap(v.id ^ d), dist)
+    }
+
+    val clusters = mutable.Set.empty[CVertex]
+    clusters ++= vertices
+
+    for(spacing <- 0 until  minSpacing) {
+      for(edge <- edgesWithDistance(spacing)){
+        if (edge.v1.leader != edge.v2.leader) {
+          if (edge.v1.leader.followers.size < edge.v2.leader.followers.size) {
+            clusters -= edge.v1.leader
+            edge.v2.leader.merge(edge.v1.leader)
+          } else {
+            clusters -= edge.v2.leader
+            edge.v1.leader.merge(edge.v2.leader)
+          }
+        }
+      }
+    }
+
+    clusters.size
+  }
+
   def main(args: Array[String]): Unit = {
     val g = Graph.read("clustering1.txt")
     val res = kClustering(g, 4)
     println(res._1 == 106)
+
+    val hypercube = readHypercube("clustering_big.txt")
+    println(numOfHypercubeClusters(hypercube, 3) == 6118)
   }
 
 }
