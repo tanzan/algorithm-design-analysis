008aa979f411308b2f7939f5504f15d8fe4c76ad
Serg Zubovich <serg.p.zubovich@gmail>
2016-08-24 19:47:00 +0300

    knapsack  top down implementation

--- src/main/scala/algo/dynprog/Knapsack.scala
+++ src/main/scala/algo/dynprog/Knapsack.scala
@@ -1,5 +1,6 @@
 package algo.dynprog
 
+import scala.collection.mutable
 import scala.io.Source
 
 /**
@@ -9,7 +10,7 @@ object Knapsack {
 
   case class Item(value:Int, size:Int)
 
-  def optimal(items:Seq[Item], size:Int):Int = {
+  def optimalUp(items:Seq[Item], size:Int):Int = {
     val solutions = Array.ofDim[Int](items.size + 1, size + 1)
     for(i <- 1 to items.size) {
       for(j <- 0 to size) {
@@ -21,6 +22,29 @@ object Knapsack {
     solutions(items.size)(size)
   }
 
+  case class Solution(prefix:Int, size:Int)
+
+  def optimalDown(items:Seq[Item], size:Int):Int = {
+
+    val solutions = mutable.Map[Solution, Int]()
+
+    def compute(prefix:Int, size:Int):Int = {
+      val s = Solution(prefix, size)
+      if (solutions.contains(s)) solutions(s)
+      else if (prefix < 0) 0
+      else {
+        val v1 = compute(prefix - 1, size)
+        val v2 = if (size - items(prefix).size < 0) v1
+        else compute(prefix - 1, size - items(prefix).size) + items(prefix).value
+
+        val v = Math.max(v1, v2)
+        solutions += Solution(prefix, size) -> v
+        v
+      }
+    }
+    compute(items.size - 1, size)
+  }
+
   def readItems(fileName:String):Seq[Item] = {
     (for(line <- Source.fromFile(fileName).getLines()) yield {
       val attr = line.split("\\s+")
@@ -30,7 +54,10 @@ object Knapsack {
 
   def main(args: Array[String]): Unit = {
     val items = readItems("knapsack1.txt")
-    println(optimal(items.tail, items.head.value) == 2493893)
+    println(optimalUp(items.tail, items.head.value) == 2493893)
+
+    val bibItems = readItems("knapsack_big.txt")
+    println(optimalDown(bibItems.tail, bibItems.head.value) == 4243395)
   }
 
 }
