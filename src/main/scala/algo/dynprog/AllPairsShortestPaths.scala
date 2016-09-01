324cc08cf131f7337c55b85c52bf755a590d3d38
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2016-09-01 17:11:52 +0300

    complete Floyd-Warshall

--- src/main/scala/algo/dynprog/AllPairsShortestPaths.scala
+++ src/main/scala/algo/dynprog/AllPairsShortestPaths.scala
@@ -7,9 +7,9 @@ import algo.graph.{Graph, Vertex}
   */
 object AllPairsShortestPaths {
 
-  class IDVertex(val id:Int) extends Vertex
+  case class IDVertex(id:Int) extends Vertex
 
-  implicit def createIDVertex(id:Int):IDVertex = new IDVertex(id)
+  implicit def createIDVertex(id:Int):IDVertex = IDVertex(id)
 
 
   class FWSolution(size: Int) {
@@ -39,20 +39,22 @@ object AllPairsShortestPaths {
       current(i)(j) = value
     }
 
-    def sizes:Array[Array[Int]] = current
-
-    def maxSize:Option[(Int, Int, Int)] = {
+    def minPath:Option[(Int, Int, Int)] = {
       var ms:Option[(Int, Int, Int)] = None
       for(i <- 0 until size) {
         for(j <- 0 until size) {
-          if (i == j && current(i)(j) < 0) return None
-          if (ms.isEmpty || ms.forall(_._3 < current(i)(j))) ms = Some((i, j, current(i)(j)))
+          if (i == j && current(i)(j) < 0) {
+            return None
+          }
+          if (ms.isEmpty || ms.forall(_._3 > current(i)(j))) ms = Some((i, j, current(i)(j)))
         }
       }
       ms
     }
 
   }
+
+  final val PlusInfinity = Int.MaxValue
 
   def floydWarshall(graph:Graph[IDVertex]):FWSolution = {
 
@@ -64,9 +66,13 @@ object AllPairsShortestPaths {
         if (i == j) solution(i, j, 0) = 0
         else
           solution(i, j, 0) = graph.edge(vertices(i), vertices(j))
-            .headOption.map(_.weight).getOrElse(Int.MaxValue)
+            .headOption.map(_.weight).getOrElse(PlusInfinity)
       }
     }
+
+    def add(x:Int, y:Int) =
+      if (x == PlusInfinity || y == PlusInfinity) PlusInfinity
+      else x + y
 
     for(k <- 1 to vertices.size) {
       solution.next()
@@ -74,7 +80,7 @@ object AllPairsShortestPaths {
         for(j <- vertices.indices) {
           solution(i, j, k) = Math.min(
             solution(i, j, k - 1),
-            solution(i, k - 1, k - 1) + solution(k - 1, j, k - 1)
+            add(solution(i, k - 1, k - 1), solution(k - 1, j, k - 1))
           )
         }
       }
@@ -84,12 +90,12 @@ object AllPairsShortestPaths {
   }
 
   def main(args: Array[String]): Unit = {
-    val s1 = floydWarshall(Graph.read("apsp-g1.txt", skip = 1)).maxSize
-    println(s1)
-    val s2 = floydWarshall(Graph.read("apsp-g2.txt", skip = 1)).maxSize
-    println(s2)
-    val s3 = floydWarshall(Graph.read("apsp-g3.txt", skip = 1)).maxSize
-    println(s3)
+    val s1 = floydWarshall(Graph.read("apsp-g1.txt", skip = 1)).minPath
+    println(s1.isEmpty)
+    val s2 = floydWarshall(Graph.read("apsp-g2.txt", skip = 1)).minPath
+    println(s2.isEmpty)
+    val s3 = floydWarshall(Graph.read("apsp-g3.txt", skip = 1)).minPath
+    println(s3.forall(_._3 == -19))
   }
 
 }
