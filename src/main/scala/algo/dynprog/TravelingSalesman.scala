31e9c59de5f89c0cd24342f7c57f1f9fc3733349
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2016-09-05 12:52:30 +0300

    TSP:read locations

--- src/main/scala/algo/dynprog/TravelingSalesman.scala
+++ src/main/scala/algo/dynprog/TravelingSalesman.scala
@@ -1,8 +1,25 @@
 package algo.dynprog
 
+import scala.io.Source
+
 /**
   * Created by serg on 9/5/16.
   */
 object TravelingSalesman {
+
+  case class Location(x:Double, y:Double)
+
+  def readLocations(fileName:String):Seq[Location] =
+    (for(line <- Source.fromFile(fileName).getLines().drop(1)) yield {
+      val loc = line.split("\\s")
+      Location(loc(0).toDouble, loc(1).toDouble)
+    }).toVector
+
+
+  def main(args: Array[String]): Unit = {
+    val locations = readLocations("tsp.txt")
+    println(locations)
+    println(locations.size)
+  }
 
 }
