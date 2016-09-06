66677b2deddd48f844975cba69a2f7b590ebb1dd
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2016-09-06 19:09:51 +0300

    TSP: basic idea

--- src/main/scala/algo/dynprog/TravelingSalesman.scala
+++ src/main/scala/algo/dynprog/TravelingSalesman.scala
@@ -7,19 +7,20 @@ import scala.io.Source
   */
 object TravelingSalesman {
 
-  case class Location(id:Int, x:Double, y:Double)
+  case class Location(id:Int, x:Double, y:Double) {
+    def distance(other:Location):Double =
+      Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2))
+  }
 
-  case class Solution(locations:Set[Location], dist:Double)
-
+  case class Solution(locations:Set[Location], dist:Double, last:Location) {
+    def +(location: Location):Solution =
+      Solution(locations + location, dist + (if (locations.isEmpty) 0.0 else locations.map(_.distance(location)).min), location)
+  }
 
   def tsp(locations:Seq[Location], start:Int):Double = {
-
-    def solutions(locations: Set[Location]): Stream[Solution] = ???
-      //locations.toStream.foldLeft(Set(Solution(Set.empty, Double.PositiveInfinity)).toStream) ((a, e) => a.flatMap(s => Set(s) ++ Set(s + e)))
-
-    solutions(locations.toSet)
+    locations.toStream.foldLeft(Set(Solution(Set(locations(start)), 0, locations(start))).toStream) ((a, e) => a.flatMap(s => Set(s) ++ Set(s + e)))
       .filter(loc => loc.locations.size  == locations.size - 1 && loc.locations.exists(_.id == start))
-      .minBy(_.dist).dist
+      .map(solution => solution.dist + solution.last.distance(locations(start))).min
   }
 
   def readLocations(fileName:String):Seq[Location] =
@@ -33,6 +34,7 @@ object TravelingSalesman {
     val locations = readLocations("tsp.txt")
     println(locations)
     println(locations.size)
+    println(tsp(locations, 1))
   }
 
 }
