079242c461958335c6cd643f67c95317fdfd7815
Serg Zubovich <sergiy.zubovich@sintez.co.za>
2016-09-16 13:37:52 +0300

    2SAT: fix

--- src/main/scala/algo/localsearch/TwoSAT.scala
+++ src/main/scala/algo/localsearch/TwoSAT.scala
@@ -11,18 +11,16 @@ object TwoSAT {
 
   val random = Random
 
-  trait Var {
+  trait Term {
 
     def apply(): Boolean
-
-    def update(value: Boolean): Unit
 
     def flip():Unit
 
     def onChange(action: => Unit):Unit
   }
 
-  class Instance extends Var {
+  class Var extends Term {
 
     private var value = false
 
@@ -30,35 +28,39 @@ object TwoSAT {
 
     override def apply(): Boolean = value
 
-    override def update(value: Boolean): Unit = {
-      this.value = value
-      actions.foreach(_())
-    }
+    def update(value: Boolean): Unit =
+      if (this() != value){
+        this.value = value
+        actions.foreach(_())
+      }
 
     override def flip():Unit = this() = !value
 
     override def onChange(action: => Unit): Unit = actions += (() => action)
   }
 
-  class Not(val variable:Var) extends Var {
+  class Not(val variable:Var) extends Term {
 
     override def apply(): Boolean = !variable()
-
-    override def update(value: Boolean): Unit =  variable() = value
 
     override def flip(): Unit = variable.flip()
 
     override def onChange(action: => Unit): Unit = variable.onChange(action)
   }
 
-  class Clause(val x:Var, val y:Var, expression: Expression) {
+  class Clause(val x:Term, val y:Term, expression: Expression) {
 
     x.onChange(updateExpression())
     y.onChange(updateExpression())
 
+    private var oldValue = this()
+
     private def updateExpression():Unit =
-      if (this()) expression._unsatisfied -= this
-      else expression._unsatisfied += this
+      if (this() != oldValue) {
+        if (this ()) expression._unsatisfied -= this
+        else expression._unsatisfied += this
+        oldValue = this()
+      }
 
     def randomFlip():Unit =
       if (random.nextBoolean()) x.flip()
@@ -73,7 +75,7 @@ object TwoSAT {
 
     private val _clauses = mutable.ArrayBuffer[Clause]()
 
-    private[TwoSAT] val _unsatisfied = mutable.Set[Clause]()
+    private[TwoSAT] val _unsatisfied = mutable.HashSet[Clause]()
 
     val vars:Iterable[Var] = varMap.values
 
@@ -82,34 +84,42 @@ object TwoSAT {
     val unsatisfied:Iterable[Clause] = _unsatisfied
 
     def addClause(x:Int, y:Int):Unit = {
-      val xv = varMap.getOrElseUpdate(Math.abs(x), new Instance())
-      val yv = varMap.getOrElseUpdate(Math.abs(y), new Instance())
+      val xv = varMap.getOrElseUpdate(Math.abs(x), new Var())
+      val yv = varMap.getOrElseUpdate(Math.abs(y), new Var())
 
       val clause = new Clause(if (x > 0) xv else new Not(xv), if (y > 0) yv else new Not(yv), this)
       _clauses += clause
-      _unsatisfied +=  clause
+      if (!clause()) _unsatisfied +=  clause
     }
 
-    def apply():Boolean = unsatisfied.isEmpty
+    def apply():Boolean =_unsatisfied.isEmpty
   }
 
   def papadimitrou(expr: Expression):Boolean = {
 
-    for(i <- 1 to (Math.log(expr.vars.size)/Math.log(2)).toInt){
+    val m = (Math.log(expr.vars.size)/Math.log(2)).toLong
+    val n = 2L*expr.vars.size.toLong*expr.vars.size.toLong
+
+    var i = 0L
+
+    while(i < m){
       expr.vars.foreach(_() = random.nextBoolean())
-      for(j <- 1 to 2*expr.vars.size*expr.vars.size) {
+      var j = 0
+      while (j < n) {
         if (expr()) return true
         expr.unsatisfied.head.randomFlip()
+        j += 1
       }
+      i += 1
     }
 
     false
   }
 
-  def readClauses(fileName:String):Expression = {
+  def readExpression(fileName:String):Expression = {
     val expr = new Expression
     Source.fromFile(fileName).getLines().drop(1).foreach{ line =>
-      val vars = line.split("\\s+").map(_.toInt)
+      val vars = line.split("\\s").map(_.toInt)
       expr.addClause(vars(0), vars(1))
     }
     expr
@@ -117,22 +127,22 @@ object TwoSAT {
 
 
   def main(args: Array[String]): Unit = {
-    val cs1 = readClauses("2sat1.txt")
+    val cs1 = readExpression("2sat1.txt")
     println(papadimitrou(cs1) == true)
 
-    val cs2 = readClauses("2sat2.txt")
+    val cs2 = readExpression("2sat2.txt")
     println(papadimitrou(cs2) == false)
 
-    val cs3 = readClauses("2sat3.txt")
+    val cs3 = readExpression("2sat3.txt")
     println(papadimitrou(cs3) == true)
 
-    val cs4 = readClauses("2sat4.txt")
+    val cs4 = readExpression("2sat4.txt")
     println(papadimitrou(cs4) == true)
 
-    val cs5 = readClauses("2sat5.txt")
+    val cs5 = readExpression("2sat5.txt")
     println(papadimitrou(cs5) == false)
 
-    val cs6 = readClauses("2sat6.txt")
+    val cs6 = readExpression("2sat6.txt")
     println(papadimitrou(cs6) == false)
 
     //101100
